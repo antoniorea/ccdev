59031e80ed2154586087889747f516dffc7812ea
Antonio Rea <antonioreacifuentes@yahoo.es>
2025-11-24 12:41:47 +0100

    Programa en C++ para operar con numeros complejos segunda version

--- complex01.cpp
+++ complex01.cpp
@@ -4,8 +4,8 @@
 $ ./complex01 
  Introduzca mod y arg del primer num complejo : 12.041 48.36
  Introduzca mod y arg del segundo num complejo : 8.60 54.46
-z1 + z2 :       El modulo de z es :20.61 el argumento z en grados es :50.90
-z1 - z2 :       El modulo de z es :3.61 el argumento z en grados es :33.69
+z1 + z2 :       El modulo de z es :20.61 el argumento z en grados es :50.90     (13.00,16.00)
+z1 - z2 :       El modulo de z es :3.61 el argumento z en grados es :33.69      (3.00,2.00)
 */
 
 #include <iostream>
